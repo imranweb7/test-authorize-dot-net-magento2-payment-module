7cade6edfe5bb3c5355e9391f3973ec280e0fd34
Muhammed Imran Hussain <imranweb7@gmail.com>
2018-11-25 15:06:04 -0800

    Define payment module composer

---
+++ Model/AuthorizeDotNet.php
@@ -0,0 +1,15 @@
+<?php
+namespace Imranweb7\AuthorizeDotNet\Model;
+
+class AuthorizeDotNet extends \Magento\Payment\Model\Method\Cc
+{
+    public function capture(\Magento\Payment\Model\InfoInterface $payment, $amount)
+    {
+        //todo add functionality later
+    }
+
+    public function authorize(\Magento\Payment\Model\InfoInterface $payment, $amount)
+    {
+        //todo add functionality later
+    }
+}
---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Imranweb7_AuthorizeDotNet',
+        __DIR__
+    );
